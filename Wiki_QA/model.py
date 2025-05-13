1b821d43a5c1259da479c977a8984968cbdb6a84
Mohit <bluesky5april2021@gmail.com>
2025-05-13 18:16:36 +0530

    defined all models for nlp tasks

---
+++ Wiki_QA/model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import math
+
+from src.blocks.encoder import Encoder_Layer
+from src.blocks.decoder import Decoder_Layer
+from src.layers.PositionalEmbeddings.sinosuidal import SinusoidalEmbeddingLayer
+
+class QA_Generator(nn.Module):
+    def __init__(self, config):
+        super(QA_Generator, self).__init__()
+
+        self.embedding_layer = SinusoidalEmbeddingLayer(config.vocab_size, config.embed_dim, config.max_length, config.device)
+        
+        self.decoder_layers = nn.ModuleList([Decoder_Layer(config) for _ in range(config.num_layers)])
+        
+        self.fc_out = nn.Linear(config.embed_dim, config.vocab_size)        # Final linear layer
+    
+    def forward(self, input):
+        input = self.embedding_layer(input)                                    # Shape: (batch, seq_len, embed_dim)
+
+        for decoder_layer in self.decoder_layers:
+            input, attention_output = decoder_layer(input, input)
+
+        output = self.fc_out(input)
+        return output, attention_output
